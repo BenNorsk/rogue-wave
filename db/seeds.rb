c71e961b39f62f420ae5660cc7d9a9fa83455664
Stian Johansen <stian.a.johansen@gmail.com>
2020-09-28 17:47:14 +0200

    Fixed my seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,16 +2,22 @@ require 'faker'
 require 'betterlorem'
 
 puts 'Deleting all records'
-Boat.destroy_all
+
 puts 'done'
+
+Address.create(city: 'oslo')
+  address = Address.last.id
+
+User.create(first_name: 'Ben', last_name: 'Reddik', email: 'ben@me.com', password: 'topsecret', address_id: address)
+  user = User.last.id
 
 50.times do
   Boat.create(
-    name: Faker::Name.unique.name,
+    name: Faker::Name.name,
     description: BetterLorem.p(1, true),
     price_per_day: Faker::Number.within(range: 1000..2000),
-    user_id: Faker::Number.within(range: 1..99).uniq,
-    address_id: Faker::Number.within(range: 100..200).uniq
+    user_id: user,
+    address_id: address
   )
 end
 
