0178ded12b318811b81918a15f3b0ce4d95c351f
Thomas Murray <thomas.murray09@gmail.com>
2020-09-28 18:25:34 +0200

    added more validations to the models

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,4 +1,7 @@
 class Boat < ApplicationRecord
   belongs_to :user
-  belongs_to :address
+  has_many :bookings, dependent: :destroy
+
+  # this assumes that there is a 1:1 relationship between the boat and the address
+  belongs_to :address, dependent: :destroy
 end
