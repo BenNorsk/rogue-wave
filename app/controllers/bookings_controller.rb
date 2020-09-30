4bebee80ca475995a183f88964e83b9ce13a6baf
Benjamin Bruckner <benjamin.brueckner@bluewin.ch>
2020-09-30 08:49:53 +0200

    update

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,7 +1,3 @@
 class BookingsController < ApplicationController
   # skip_before_action :authenticate_user!, only: %i[home playground]
-  def index
-    @bookings = Booking.where("user_id = #{current_user.id}")
-    raise
-  end
 end
