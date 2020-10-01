8419a3d09f89c6e664e36813049a64fd8a582ac6
Thomas Murray <thomas.murray09@gmail.com>
2020-10-01 21:20:13 +0200

    js cleanup

--- app/javascript/plugins/typed_banner.js
+++ app/javascript/plugins/typed_banner.js
@@ -1,11 +1,16 @@
 import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
-  new Typed('#banner-typed-text', {
-    strings: ["...let your dreams set sail.^4000"],
-    typeSpeed: 80,
-    loop: true
-  });
+  // check if there is some text to animate
+  const bannerText = document.getElementById('banner-typed-text');
+
+  if (bannerText) {
+    new Typed('#banner-typed-text', {
+      strings: ["...let your dreams set sail.^4000"],
+      typeSpeed: 80,
+      loop: true
+    });
+  }
 };
 
 document.addEventListener('turbolinks:load', () => {
