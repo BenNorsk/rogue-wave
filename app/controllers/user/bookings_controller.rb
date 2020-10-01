d2372d9f5cabb93d083450cb0a98749264d2e08f
Benjamin Bruckner <benjamin.brueckner@bluewin.ch>
2020-10-01 16:51:00 +0200

    update on booking card: functionality added for boat_owner

--- app/controllers/user/bookings_controller.rb
+++ app/controllers/user/bookings_controller.rb
@@ -2,4 +2,20 @@ class User::BookingsController < ApplicationController
   def index
     @bookings = Booking.where("user_id = #{current_user.id}")
   end
+
+  def update
+    @booking = Booking.find(params["id"])
+    if @booking.update(booking_params)
+      redirect_to("/playground", notice: "Your booking status has been successfully updated.")
+    else
+      redirect_to("/playground", alert: "Your booking status could not have been updated.")
+    end
+
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:status)
+  end
 end
